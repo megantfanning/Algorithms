6c18e4654bee6a3f8b4e9d29b973e350210db07a
Megan Fanning <megantfanning@gmail.com>
2016-04-18 12:31:34 -0700

    fixed greedy algorithm

--- MakeChange/Algos.c
+++ MakeChange/Algos.c
@@ -9,7 +9,9 @@
 // Programs accept
 //An array V where V[i] is the value of the coin of the ith denomination.
 // A value A which is the amount of change we are asked to make
-//return an array of length 4 (pennies, nickels, dimes and quarters.
+
+//Algorithms return
+// The algorithm should return an array C where C[i] is the number of coins of value V[i] to return as change and m the minimum number of coins it took. 
 
 //Brute force/Divide and Conquer
 int * changeslow(int V[],int A){
@@ -25,16 +27,20 @@ int * changeslow(int V[],int A){
 //Greedy
 int * changegreedy(int V[],int A){
     int i=0;
-    int coins=0;
-    while(coins != A){
+    int totalChange=0;
+    int MyCoins[5]={0,0,0,0,0};//TODO fix
+    int minCoins=0;
+    while(totalChange != A){
         //set i to length of the array
         i=sizeof(V) / sizeof(V[0]);
         //Use the largest value coin possible.
         if(V[i] > 0){
             //check if the value of the coin to ensure we don't give too much change
-            if( i+coins < A){
+            if( i+totalChange < A){
                 coins= i + coins;
                 V[i] = V[i]-1;
+                minCoins++;//incriment the number of total coins used
+                myCoins[i]=myCoins[i]+i;//incriment my array
                 //Subtract the value from the amount of change to be made.
             }else{
                 //if the value of i is too great to make change decrease to smaller size coin denomination
@@ -44,8 +50,9 @@ int * changegreedy(int V[],int A){
             i--;
         }
     }
-
 }
+
+int B[4]{3,0,0,0,}
 
 //Dynamic
 int * changedp(int V[],int A){}
