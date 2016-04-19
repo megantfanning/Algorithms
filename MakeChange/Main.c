8d75829b18b71328316c0add32fbdf89766e2006
Ian Dalrymple <dalrympi@flip2.engr.oregonstate.edu>
2016-04-19 15:11:22 -0700

    Initial checks for command line args - just starting on IO

--- MakeChange/Main.c
+++ MakeChange/Main.c
@@ -1,9 +1,9 @@
 /* ------------------------------------------------------------------------- *
 // Project Name: 	CS325 - Project 2
-// File 			Name: main.c
+// File 			Name: Main.c
 // Desc: 			Entry point for project.
 // Authors:			Ian Dalrymple, Megan Fanning, Toni York
-// Date: 			04/18/2016
+// Date: 			04/25/2016
 /* -------------------------------------------------------------------------*/
 
 // Includes 
@@ -11,14 +11,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "dynamicArray.h"
-#include "UserInt.h"
-#include "Algos.h"
-
-// Main Entry Point 
-int main (int argc, const char * argv[]) 
-{
-    printf("Making Change\n");
+//#include "dynamicArray.h"
+//#include "UserInt.h"
+//#include "Algos.h"
 
 /*
 The execution of the program(s) should be as follows:
@@ -27,17 +22,41 @@ The execution of the program(s) should be as follows:
 The next line contains one integer value for which we must make change.
 Program output should be written to a file named [input filename]change.txt where [input filename].txt is the input filename, and should be formatted with the name of the algorithm used, the change result and the minimum number of coins m, per line.
 */
-    //define these variables using file input
-    int A;
-    int V[4]={3,2,1,1,1};
+// Main Entry Point 
+int main (int argc, const char * argv[]) 
+{
+	// Local declares 
+	const char* inFile;
+	
+	// Check for correct command line input from user
+    if(argc == 1)
+	{	
+		// Tell the user what is happening
+        printf("Include the file name as an argument to the command line. \n");	
+		
+		// Return fail to os 
+		return -99;
+	}
+    else if(argc == 2)
+	{
+		// Get the input file name 
+		inFile = argv[1];
+		printf("%s ", inFile);
+		return 0;
+	}
 
-    //call agorithms
-    //Brute force/Divide and Conquer
-    int Change[]= changeslow(V[],A){}
-    //Greedy
-    int Change[]= changegreedy(V[],A){}
-    //Dynamic
-    int Change[]= changedp(V[],A){}
+
+    // //define these variables using file input
+    // int A;
+    // int V[4]={3,2,1,1,1};
+
+    // //call agorithms
+    // //Brute force/Divide and Conquer
+    // int Change[]= changeslow(V[],A){}
+    // //Greedy
+    // int Change[]= changegreedy(V[],A){}
+    // //Dynamic
+    // int Change[]= changedp(V[],A){}
 }
 
 
