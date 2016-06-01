f20b46f2974c1b09e6bed65d69a31c9d79c0292d
Megan Fanning <megantfanning@gmail.com>
2016-06-01 16:02:41 -0700

    Added get distance function

--- TSP/IO/Algos.c
+++ TSP/IO/Algos.c
@@ -10,6 +10,29 @@
 #include <stdio.h>
 #include "Algos.h"
 
+
+
+int getDistance(struct structCity A, struct structCity B){
+    //get distance between two cities
+    int x=A[x]-B[x];//subtract x coordinates
+    int y=A[x]-B[y];//subtract y coordinates
+    int dist=pow(y,2)+pow(x,2);// raise both to the power of two and add
+    int distance=sqrt(dist);//sqrt of the sum of x^2+y^2
+    distance=nearestint(distance);//round to nearest int
+    return Distance;
+}
+
+
+int NearestNeightbor(struct structCity *input, int *output, int size){
+
+    //start on an arbitrary vertex as current vertex.
+    //2. find out the shortest edge connecting current vertex and an unvisited vertex V.
+    //set current vertex to V.
+    //mark V as visited.
+    //if all the vertices in domain are visited, then terminate.
+    //else Go to step 2.
+}
+
 // Algorithm to get the shortest round trip for the 
 // input array passed in. The algorithm should populate 
 // the output array that is passed in with the first row 
--- TSP/TSP.c
+++ TSP/TSP.c
@@ -1,9 +1,20 @@
  //Team 8: Megan Fanning,Toni York , Ian Dalrymple
 
-//function reads in a problem file
-int readFileIn(){}
+/*
+x=x1-x2
+y=y1-y2
+dist=y^2+x^2
+distance=nearestint(distance)=sqrt (dist)
 
-//function takes solution and prints it out to a file
-int printSolutionOut(){}
+*/
 
-int main(){}
+
+
+int main(){
+    //start on an arbitrary vertex as current vertex.
+    //2. find out the shortest edge connecting current vertex and an unvisited vertex V.
+    //set current vertex to V.
+    //mark V as visited.
+    //if all the vertices in domain are visited, then terminate.
+    //else Go to step 2.
+}
