1311e7cd24db529d809721a6d92d7491ec3f3aba
Megan Fanning <megantfanning@gmail.com>
2016-06-02 18:35:45 -0700

    fixed visited cities for dist to finish loop

--- TSP/IO/Algos.c
+++ TSP/IO/Algos.c
@@ -130,7 +130,7 @@ int resultTSP(struct structCity *input, int *output, int size, int StartNode)
 		output[i] = visitedCities[i].iId;
 	}
     // add the distance from the last city to the first city;
-    int distToFinishLoop=getDistance(visitedCities[0],visitedCities[size]); 	
+    int distToFinishLoop=getDistance(visitedCities[1],visitedCities[size]); 	
     TotalDist+=distToFinishLoop;
 	// Move the total distance traveled to index 0 in the output list
 	output[0] = TotalDist;
